3fc21fc5e19ef2cfffb6bb3ee89858b91db7ffeb
c3ntum <164254866+c3ntum@users.noreply.github.com>
2025-01-14 15:43:32 +0100

    Add files via upload

--- footbar.js
+++ footbar.js
@@ -4,7 +4,11 @@ const footerContainer = document.getElementById('footer-container');
 // Footer HTML dynamisch erstellen
 const footerHTML = `
 <footer class="bg-dark text-center text-white py-4">
-    <p>&copy;2025 c3ntum. All rights reserved</p>
+    <div class="d-flex justify-content-center align-items-center">
+        <p class="mb-0">&copy;2025 c3ntum. All rights reserved</p>
+        <a href="disclaimer.html" class="ms-2 text-white text-decoration-underline">Disclaimer</a>
+        <a href="copyright.html" class="ms-2 text-white text-decoration-underline">Copyright</a>
+    </div>
 </footer>
 `;
 
--- menubar.js
+++ menubar.js
@@ -4,7 +4,7 @@ const navbarContainer = document.getElementById('navbar-container');
 // Navbar HTML dynamisch erstellen
 const navbarHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">c3ntum</a>
+    <a class="navbar-brand" href="index.html">c3ntum</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -17,7 +17,7 @@ const navbarHTML = `
                 <a class="nav-link" href="invoice.html">Invoice</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="about.html">About</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
